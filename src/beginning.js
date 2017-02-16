8cd57169f4088ed1137cfaee020cdc60d8c2a45f
Vespera Hiems <vespera.hiems@gmail.com>
2017-02-16 15:51:57 +0200

    Added notes about player data

--- src/beginning.js
+++ src/beginning.js
@@ -4,9 +4,20 @@ var beginning_state = {
     create: function () {
         game.add.sprite(0, 0, 'background');
         var style = {font: "25px verdana", fill: "#FFFFFF"};
+        
+        //all the options for player creation
+        var sexes = ["male", "female"]; //probability based on statistics
+        var maleNames = ["Eino", "Toivo", "Veikko", "Väinö", "Tauno", "Erkki"]; //purely random based on sex
+        var femaleNames = ["Anna", "Aino", "Aili", "Tyyne", "Aune", "Helvi"]; //purely random based on sex
+        var classes = ["poor", "middle", "rich"]; //probability based on statistics
+        var locations = ["country", "city"]; //probability based on statistics
+        
+        //todo: create player attributes based on probability
+        var player = {name: "Vilho", sex: "male", class: "poor", location: "country"};
+        
         var text1 = game.add.text(game.world.centerX * 0.3, game.world.centerY * 0.5, "Synnyit poikavauvaksi vuoteen 1917.", style);
-        var text2 = game.add.text(game.world.centerX * 0.3, game.world.centerY * 0.6, "Nimesi on Vilho.", style);
-        var text3 = game.add.text(game.world.centerX * 0.3, game.world.centerY * 0.7, "Elät köyhässä perheessä.", style);
+        var text2 = game.add.text(game.world.centerX * 0.3, game.world.centerY * 0.6, "Nimesi on "+player.name+".", style);
+        var text3 = game.add.text(game.world.centerX * 0.3, game.world.centerY * 0.7, "Elät köyhässä perheessä maaseudulla.", style);
 
         var continueButton = game.add.button(game.world.centerX, game.world.centerY, 'button');
         continueButton.anchor.set(0.5);
@@ -21,6 +32,7 @@ var beginning_state = {
 }
 
 function goToSchool() {
+    //x% die before going to school
+    //country children receive 20% chance to not go to school
     game.state.start('school');
-
 }
