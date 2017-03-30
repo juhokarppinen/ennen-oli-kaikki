38ee357c3b2add5e1339eca4adbfbb2ceb101ba6
Vespera Hiems <vespera.hiems@gmail.com>
2017-03-30 21:58:35 +0300

    Small text change

--- src/homechores2.js
+++ src/homechores2.js
@@ -4,9 +4,9 @@ var homechores2_state = {
         PLAYER.age = 16;
 
         if(PLAYER.gender === "male") {
-            var text = "Raadat isäsi rinnalla kotitilalla, muttet voi lakata " +
-                       "jossittelemasta, millaiseksi elämäsi olisi muuttunut, " +
-                       "jos olisit jatkanut opintoja.";
+            var text = "Kasvat aikuiseksi isäsi rinnalla, töitä tehden. Haaveilet edelleen opiskelusta, "
+            +"mutta päivät pellolla vievät kaiken voimasi, etkä jaksa lukea iltaisin, "
+            +"vaikka vanha opettajasi sinulle auliisti kirjojaan lainaakin.";
         } else {
             var text = "Vuosien kuluessa saat lisää vastuuta ja pääset auttamaan "+
                        "tilan hoitamiseen liittyvissä tehtävissä.";
