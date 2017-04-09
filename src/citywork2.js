1737ae03fa88e7335829d9b13173bb314bccbfe9
Vespera Hiems <vespera.hiems@gmail.com>
2017-04-09 17:28:42 +0300

    Fixed a bug concerning a country girl from Saarijärvi

--- src/citywork2.js
+++ src/citywork2.js
@@ -2,7 +2,8 @@ var citywork2_state = {
 
     create: function() {
         PLAYER.age = 13;
-        var text = "Mikä onni, sait kuin saitkin paikan makeistehtaalta. Tehtaalle on juuri hankittu uusi lakritsikone jonka välittömässä läheisyydessä tulet viettämään vielä monta vuotta.";
+        var text = "Mikä onni, sait kuin saitkin paikan makeistehtaalta. Tehtaalle on juuri hankittu uusi lakritsikone "
+        +"jonka välittömässä läheisyydessä tulet viettämään vielä monta vuotta.";
 
         var image               = 'candyfactorybackground';
         var centerButtonLabel   = "Jatka";
