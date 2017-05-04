2d17ac52b1d506c0a144ac5400c2b87e82b678d3
Vespera Hiems <vespera.hiems@gmail.com>
2017-05-04 14:47:22 +0300

    Fixed typo

--- src/load.js
+++ src/load.js
@@ -83,7 +83,7 @@ var load_state = {
         GAME.load.image('warbackground', 'assets/img/war.jpg');
         GAME.load.image('warworkbackground', 'assets/img/warwork.jpg');
         GAME.load.image('lottawarbackground', 'assets/img/lottawar.jpg');
-        GAME.load.image('consctructionbackground', 'assets/img/construction.jpg');
+        GAME.load.image('constructionbackground', 'assets/img/construction.jpg');
         //articles
         GAME.load.image('articlewinterwar', 'assets/img/articlewinterwar.jpg');
         GAME.load.image('articlepeace', 'assets/img/articlepeace.jpg');
