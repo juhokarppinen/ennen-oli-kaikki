dc71862436f8be463cd83e33e387d22a25667ab7
Vespera Hiems <vespera.hiems@gmail.com>
2017-05-04 15:14:18 +0300

    Added one more step to torppa saga

--- src/citywork1.js
+++ src/citywork1.js
@@ -1,32 +1,39 @@
 var citywork1_state = {
 
     create: function() {
-        PLAYER.age = 16;
-        PLAYER.movedtocity = true;
-        if(PLAYER.class === "poor") {
-            if(PLAYER.gender === "male") {
-                var image     = 'riflefactorybackground';
-                var text = "Lähtö kannatti! Saat töitä Valtion kivääritehtaalta. Palkkasi on parempi kuin renkinä, "
-                +"mutta toisaalta joudut maksamaan asumisesta toisin kuin renkiaikoina. "
-                +"Työ on raskasta tehtaallakin, mutta olet silti tyytyväinen maisemanvaihdokseen.";
-                PLAYER.job = "riflefactory";
-            } else {
-                var image     = 'homeassistantbackground';
-                PLAYER.widowboss = true;
-                var text = "Lähtö kannatti! Saat kotiapulaisen paikan ikääntyvän leskirouvan luota. "
-                +"Hän vaatii sinulta paljon, mutta olet tyytyväinen, sillä palkkasi riittää vaatteisiin, joista et aiemmin uskaltanut edes haaveilla.";
-            }
+        if(PLAYER.movedtocity === true) {
+            var image     = 'homeassistantbackground';
+            PLAYER.widowboss = true;
+            var text = "Työsi ikääntyvän leskirouvan kotiapulaisena ei juuri poikkea töistäsi kotona, "
+            +"mutta saat uutta sisältöä elämääsi vasta perustetusta Jyväskylän kansalaisopistosta, jossa opiskelet vapaa-ajallasi englantia ja kuorolaulua.";
         } else {
-            if(PLAYER.gender === "male") {
-                var image     = 'plywoodfactorybackground';
-                var text = "Vaikka valinta oli vaikea, et ole katunut sitä hetkeäkään. Työsi vaneritehtaalla on rankkaa, "
-                +"mutta saat uutta sisältöä elämääsi vasta perustetusta Jyväskylän kansalaisopistosta, jossa opiskelet vapaa-ajallasi laskentoa ja koneenpiirustusta.";
-                PLAYER.job = "plywoodfactory";
+            PLAYER.age = 16;
+            PLAYER.movedtocity = true;
+            if(PLAYER.class === "poor") {
+                if(PLAYER.gender === "male") {
+                    var image     = 'riflefactorybackground';
+                    var text = "Lähtö kannatti! Saat töitä Valtion kivääritehtaalta. Palkkasi on parempi kuin renkinä, "
+                    +"mutta toisaalta joudut maksamaan asumisesta toisin kuin renkiaikoina. "
+                    +"Työ on raskasta tehtaallakin, mutta olet silti tyytyväinen maisemanvaihdokseen.";
+                    PLAYER.job = "riflefactory";
+                } else {
+                    var image     = 'homeassistantbackground';
+                    PLAYER.widowboss = true;
+                    var text = "Lähtö kannatti! Saat kotiapulaisen paikan ikääntyvän leskirouvan luota. "
+                    +"Hän vaatii sinulta paljon, mutta olet tyytyväinen, sillä palkkasi riittää vaatteisiin, joista et aiemmin uskaltanut edes haaveilla.";
+                }
             } else {
-                var image     = 'homeassistantbackground';
-                PLAYER.widowboss = true;
-                var text = "Vaikka valinta oli vaikea, et ole katunut sitä hetkeäkään. Työsi leskirouvan kotiapulaisena ei juuri poikkea töistäsi kotona, "
-                +"mutta saat uutta sisältöä elämääsi vasta perustetusta Jyväskylän kansalaisopistosta, jossa opiskelet vapaa-ajallasi englantia ja kuorolaulua.";
+                if(PLAYER.gender === "male") {
+                    var image     = 'plywoodfactorybackground';
+                    var text = "Vaikka valinta oli vaikea, et ole katunut sitä hetkeäkään. Työsi vaneritehtaalla on rankkaa, "
+                    +"mutta saat uutta sisältöä elämääsi vasta perustetusta Jyväskylän kansalaisopistosta, jossa opiskelet vapaa-ajallasi laskentoa ja koneenpiirustusta.";
+                    PLAYER.job = "plywoodfactory";
+                } else {
+                    var image     = 'homeassistantbackground';
+                    PLAYER.widowboss = true;
+                    var text = "Vaikka valinta oli vaikea, et ole katunut sitä hetkeäkään. Työsi leskirouvan kotiapulaisena ei juuri poikkea töistäsi kotona, "
+                    +"mutta saat uutta sisältöä elämääsi vasta perustetusta Jyväskylän kansalaisopistosta, jossa opiskelet vapaa-ajallasi englantia ja kuorolaulua.";
+                }
             }
         }
         
