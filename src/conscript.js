f07b79487f07487c98b48ccea5d1fcf91d2ff5b1
juhokarppinen <juho.karppinen@gmail.com>
2017-05-04 23:10:39 +0300

    Add info to conscript and noconscript

--- src/conscript.js
+++ src/conscript.js
@@ -2,7 +2,7 @@ var conscript_state = {
 
     create: function() {
         PLAYER.age = 20;
-        var text = "Saat valtiolta kirjeen: Sinun on ilmoittauduttava asepalvelukseen.";
+        var text = "Saat valtiolta kirjeen: sinun on ilmoittauduttava asepalvelukseen.";
         if(PLAYER.improvement === true) {
             text += " Suunnittelemasi pappilan laajennos lykkäytyy.";
         }
@@ -15,8 +15,11 @@ var conscript_state = {
         
         var image            = 'conscriptbackground';
         var centerButtonLabel = "Jatka";
+        var infoText = "Kutsu asepalvelukseen tuli sinä vuonna, kun täytti 20. Asepalvelus kesti 350 päivää. " +
+        "Aseettomana oli mahdotonta palvella ennen vuotta 1969, jolloin siviilipalveluksen mahdollistanut laki " +
+        "tuli voimaan.\n\nLähteet: 1) Helsingin kaupunginkirjasto, kysy.fi 2) Arkistojen Portti. Palvelus armeijassa. wiki.narc.fi";
 
-        drawUIsingleButton(image, text, centerButtonLabel, this);
+        drawUIsingleButton(image, text, centerButtonLabel, this, infoText);
     },
 
     centerButtonHandler: function () {
--- src/noconscript.js
+++ src/noconscript.js
@@ -3,15 +3,21 @@ var noconscript_state = {
     create: function() {
         PLAYER.age = 20;
         PLAYER.noconscript = true;
-        var text = "Saat valtiolta kirjeen: Sinun on ilmoittauduttava asepalvelukseen. Kutsuntatilaisuudessa selviää, "
+        var text = "Saat valtiolta kirjeen: sinun on ilmoittauduttava asepalvelukseen. Kutsuntatilaisuudessa selviää, "
         +"että olet liian heikkountoinen suorittaaksesi asepalveluksen. Lapsuutesi "
         +"heikko ravinto on jättänyt jälkensä kehoosi, eikä kutsuntalääkäri usko "
         +"riisitaudin heikentämien luittesi kestävän varusmiehen elämää.";
         
         var image            = 'conscriptbackground';
         var centerButtonLabel = "Jatka";
+        var infoText = "Kutsu asepalvelukseen tuli sinä vuonna, kun täytti 20. Asepalvelus kesti 350 päivää. " +
+        "Aseettomana oli mahdotonta palvella ennen vuotta 1969, jolloin siviilipalveluksen mahdollistanut laki " +
+        "tuli voimaan. Viidesosa ikäluokasta hylättiin asepalveluksesta kutsunnoissa. Taustalla olivat pääosin " +
+        "terveydelliset syyt. Luku oli korkeampi kuin muissa Pohjoismaissa." +
+        "\n\nLähteet: 1) Helsingin kaupunginkirjasto, kysy.fi 2) Arkistojen Portti. Palvelus armeijassa. wiki.narc.fi " +
+        "3) Suomalaisten elintaso koheni itsenäisyyden kahdella ensimmäisellä vuosikymmenellä. Artikkeli. itsenaisyys100.fi";
 
-        drawUIsingleButton(image, text, centerButtonLabel, this);
+        drawUIsingleButton(image, text, centerButtonLabel, this, infoText);
     },
 
     centerButtonHandler: function () {
