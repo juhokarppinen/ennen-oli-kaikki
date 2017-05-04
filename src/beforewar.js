d110fcc6e938d026b3c9695e18876a0b8cb0189e
Vespera Hiems <vespera.hiems@gmail.com>
2017-05-04 15:21:24 +0300

    Fixed widowboss war text

--- src/beforewar.js
+++ src/beforewar.js
@@ -11,16 +11,14 @@ var beforewar_state = {
             +"kutsutaan ylimääräisiin kertaushajoituksiin. Myös veljesi saa kutsun. "
             +"Isäsi yrittää lohduttaa nyyhkyttävää äitiäsi sanomalla, että ehkä kyse "
             +"tosiaankin on vain harjoituksista, mutta kuulet huolen hänen äänestään.";
+        } else if(PLAYER.widowboss === true) {
+            var text = "Kaupungin kadut hiljenevät lokakuussa 1939, kun kaikki reserviläiset "
+            +"kutsutaan ylimääräisiin kertaushajoituksiin. Myös leskirouvan poika saa kutsun, vaikka on jo lähes 40-vuotias. "
+            +"Yrität lohduttaa nyyhkyttävää rouvaa parhaasi mukaan.";
         } else if(PLAYER.movedtocity === true) {
             var text = "Kaupungin kadut hiljenevät lokakuussa 1939, kun kaikki reserviläiset "
-            +"kutsutaan ylimääräisiin kertaushajoituksiin.";
-            if(PLAYER.widowboss === true) {
-                text += " Myös leskirouvan poika saa kutsun, vaikka on jo lähes 40-vuotias. "
-                +"Yrität lohduttaa nyyhkyttävää rouvaa parhaasi mukaan.";
-            } else {
-                text += " Myös vuokranantajasi poika saa kutsun. "
-                +"Yrität lohduttaa hänen nyyhkyttävää äitiään parhaasi mukaan.";
-            }
+            +"kutsutaan ylimääräisiin kertaushajoituksiin. Myös vuokranantajasi poika saa kutsun. "
+            +"Yrität lohduttaa pojan nyyhkyttävää äitiään parhaasi mukaan.";
         } else {
             var text = "Kylän raitilla on hiljaista lokakuussa 1939, sillä lähes kaikki seudun miehet kaikkein nuorimpia "
             +"ja vanhimpia lukuunottamatta on kutsuttu ylimääräisiin kertausharjoituksiin. Kotiin jääneiden ilmeet ovat vakavia.";
