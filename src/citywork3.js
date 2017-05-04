49dcd1cd04515b8909021744a13bbe7b8182224a
Vespera Hiems <vespera.hiems@gmail.com>
2017-05-04 15:52:51 +0300

    Fixed story branch with noschool going to bakery

--- src/citywork3.js
+++ src/citywork3.js
@@ -4,8 +4,7 @@ var citywork3_state = {
         PLAYER.age = 14;
         var text = "Luku- ja laskutaidottomana sinun ei ole helppoa löytää töitä kaupungista. "
         +"Aikasi kuluu kaupungin kaduilla norkoillen ja satunnaisia juoksupojan töitä tehden. "
-        +"Vaneritehtaalla työskentelevä tuttavasi tietää kertoa, että tehtaalla saattaisi olla "
-        +"hanttihommia tarjolla ja hän lupaa suositella sinua tehtävään.";
+        +"Onneksi kuulet että leipomossa tarvitaan juoksupoikaa, ja vikkelänä poikana saat paikan.";
 
         this.backgroundSound = GAME.add.audio('ambience-city');
         this.backgroundSound.loop = true;
@@ -20,6 +19,6 @@ var citywork3_state = {
 
     centerButtonHandler: function () {
         this.backgroundSound.stop();
-        GAME.state.start('citywork4');
+        GAME.state.start('youthwork3');
     }
 }
