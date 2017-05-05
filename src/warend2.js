3226b59546a7a07f07b8cdb778e97307cd387478
juhokarppinen <juho.karppinen@gmail.com>
2017-05-05 12:10:42 +0300

    Add info to warend2

--- src/warend2.js
+++ src/warend2.js
@@ -6,8 +6,10 @@ var warend2_state = {
         var text = "Viimeiset saksalaisjoukot vetäytyvät Norjan puolelle 27.4.1945 jättäen jälkeensä lähes maan tasalle poltetun Pohjois-Suomen.";
                 
         var centerButtonLabel = "Jatka";
+        var infoText = "Talvi- ja jatkosota (1939 - 1944) sekä Lapin sota (1944 - 1945) vaativat yhteensä noin 95 000 suomalaisen hengen." +
+            "\n\nLähde: Juutilainen, A & Leskinen, J. (toim.). Jatkosodan pikkujättiläinen.";
 
-        drawUIsingleButton(image, text, centerButtonLabel, this);
+        drawUIsingleButton(image, text, centerButtonLabel, this, infoText);
     },
 
     centerButtonHandler: function () {
